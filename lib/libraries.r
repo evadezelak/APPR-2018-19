a0444ca5a5d67fa2383612407150db91097e91fc
Eva Deželak <eva.dezelak@gmail.com>
2018-12-12 23:23:07 +0100

    začetek uvoza

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,8 +1,11 @@
+library(proto)
 library(knitr)
+library(xml2)
 library(rvest)
 library(gsubfn)
 library(reshape2)
 library(shiny)
+library(readr)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
-source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+#source("lib/uvozi.zemljevid.r", encoding="UTF-8")
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,51 +1,30 @@
 # 2. faza: Uvoz podatkov
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+sl <- locale("sl", decimal_mark=".", grouping_mark=",")
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
+
+# Funkcija, ki uvozi podatke iz datoteke turizem_glede_na_transport.csv
+uvozi.turizem_glede_na_transport <- function() {
+  data <- read_csv("podatki/turizem_glede_na_transport.csv", col_names=c("turizem_glede_na_transport"),
                     locale=locale(encoding="Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  data <- data %>% melt(id.vars="obcina", variable.name="velikost.druzine",
-                        value.name="stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- factor(data$obcina, levels=obcine)
+  data <- data[-c(1),]
+  #colnames(data)[1] <- "leto"
+  #data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
+    #strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
+  #data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
+  #data <- data %>% melt(id.vars="obcina", variable.name="velikost.druzine",
+              #          value.name="stevilo.druzin")
+  #data$velikost.druzine <- parse_number(data$velikost.druzine)
+  #data$obcina <- factor(data$obcina, levels=obcine)
   return(data)
 }
 
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
+# Zapišimo podatke v razpredelnico
+turizem_glede_na_transport <- uvozi.turizem_glede_na_transport()
 
 # Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
+#druzine <- uvozi.druzine(levels(obcine$obcina))
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
