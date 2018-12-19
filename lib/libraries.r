2dc0d6c2fc02f41b2723adfc6bd1597045a50931
Eva Deželak <eva.dezelak@gmail.com>
2018-12-19 12:24:03 +0100

    uvoz csv

--- lib/libraries.r
+++ lib/libraries.r
@@ -9,3 +9,4 @@ library(readr)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -2,21 +2,37 @@
 
 sl <- locale("sl", decimal_mark=".", grouping_mark=";")
 
-
+# Funkcija, ki uvozi občine iz Eurostata kot HTML
+uvozi.letalski_promet <- function() {
+  link <- "file:///U:/APPR-2018-19/podatki/letalski_promet.html"
+  stran <- html_session(link) %>% read_html()
+  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+    .[[1]] %>% html_table(dec=",")
+  for (i in 1:ncol(tabela)) {
+    if (is.character(tabela[[i]])) {
+      Encoding(tabela[[i]]) <- "UTF-8"
+    }
+  }
+  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
+                        "ustanovitev", "pokrajina", "regija", "odcepitev")
+  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
+  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
+  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
+  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
+    tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
+  }
+  for (col in c("obcina", "pokrajina", "regija")) {
+    tabela[[col]] <- factor(tabela[[col]])
+  }
+  return(tabela)
+}
 
 # Funkcija, ki uvozi podatke iz datoteke turizem_glede_na_transport.csv
 uvozi.turizem_glede_na_transport <- function() {
-  data <- read_csv2("podatki/turizem_glede_na_transport.csv", col_names=c("turizem_glede_na_transport"),
-                    locale=locale(encoding="Windows-1250"))
-  #data <- data[-c(1),]
-  #colnames(data)[1] <- "leto"
-  #data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    #strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  #data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  #data <- data %>% melt(id.vars="obcina", variable.name="velikost.druzine",
-              #          value.name="stevilo.druzin")
-  #data$velikost.druzine <- parse_number(data$velikost.druzine)
-  #data$obcina <- factor(data$obcina, levels=obcine)
+  data <- read_csv2("podatki/turizem_glede_na_transport.csv", na="..",
+                    locale=locale(encoding="Windows-1250"), skip=5) %>% .[-1, -1] %>%
+    melt(id.vars="COUNTRY", variable.name="leto", value.name="stevilo", na.rm=TRUE) %>%
+    mutate(leto=parse_number(leto))
   return(data)
 }
 
