464f6c71cd3311f42b6afe19120827eda62aa9bd
Eva Deželak <eva.dezelak@gmail.com>
2019-01-27 21:59:55 +0100

    shiny

--- shiny/server.R
+++ shiny/server.R
@@ -13,7 +13,7 @@ function(input, output) {
   
   
   output$zemljevid1 <- renderPlot({
-    naslov.drzave <- "Število potnikov preko letalskega prometa za posamezne države"
+    naslov.drzave <- "Število potnikov preko letalskega prometa"
     zemljevid.letalski.promet <- ggplot() +
       geom_polygon(data = left_join(zemljevid, letalski_promet %>% filter(Cetrtletje == input$leto1), 
       by = c("NAME_LONG" = "Drzava")), aes(x = long, y = lat, group = group, fill = Stevilo_potnikov/100000), color = "black")+
@@ -22,19 +22,5 @@ function(input, output) {
       labs(fill = 'Število potnikov (x 100.000)') 
     print(zemljevid.letalski.promet)
   })  
-  
-  #output$graf2 <- renderPlot({
-   # podatki <- brezposelnost_in_obsojeni2 %>% filter(obsojeni < 10 & brezposelni < 25) %>% filter(leto == input$leto2)
-    #LM <- lm(obsojeni ~ brezposelni, data=podatki)
-    #novi.brezposelni <- data.frame(brezposelni=c(25, 30,35))
-    #predict(LM, novi.brezposelni)
-    #napoved <- novi.brezposelni %>% mutate(obsojeni=predict(LM, .))
-    #graf.povezava <- ggplot(podatki, aes(x = brezposelni, y = obsojeni)) + 
-     # geom_point(shape=1) + 
-      #geom_smooth(method=lm, se = FALSE, fullrange = TRUE) +
-      #geom_point(data=napoved, aes(x = brezposelni, y = obsojeni), color='red', size=3)+
-      #labs(title = "Povezava med stopnjo brezposelnih in obsojenih")
-    #print(graf.povezava)
-  #})
   
 }
